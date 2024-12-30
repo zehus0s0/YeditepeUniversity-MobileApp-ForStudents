5710ac5d86e60815c2519175e8b79096ad0e46fd
imacapella <gurkankaradas42@gmail.com>
2024-12-30 23:59:41 +0300

    Colors Changed

--- app/src/main/java/com/example/myapplication/Utilities/Constants.kt
+++ app/src/main/java/com/example/myapplication/Utilities/Constants.kt
@@ -6,12 +6,13 @@ object Constants {
 
     //*********
     // RENKLER
-    val hubBabyBlue = Color(0xFF9EC7F2) // Bebek mavisi
+    val hubAcikYesil = Color(0xFF5BC658)
+    val hubBabyBlue = Color(0xFF85C0FF) // Bebek mavisi
+    val hubBlue = Color(0xFF3C9BFF)
     val hubBlack = Color(0xFF342E37) // Siyah
     val hubWhite = Color(0xFFF3F3F3) // Beyaz
-    val hubGreen = Color(0xFF718A39) // Fıstık Yeşili
-    val hubDarkBlue = Color(0xFF1B4965) // Koyu Mavi
     val hubGray = Color(0xFFD9D9D9) // Gri
+    val hubDark = Color(0xFF5F5464)
     //*********
 
     //*********
